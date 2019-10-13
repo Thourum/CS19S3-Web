f1c4b06b10f3ac06ccc3b25f3d0e4c058657c0d2
Samuel Asvanyi <samoasvanyi@gmail.com>
2019-10-13 13:05:46 +0200

    Initial I don't want to be bothered to fork...

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,35 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+let getEl = x => document.getElementById(x);
+
+interface Value {
+	Id: int;
+	Value: string;
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
+let arrOfValues: Value[] = [];
+let RunFunc = () => {
+	let index: number = arrOfValues.length;
+	if (!index) {
+		index = 1;
+	} else {
+		index++
+	}
+	let el = getEl("input");
+	let selector = getEl("select");
+	let val = el.value;
+	val = selector.options.selectedIndex === 0 ? val.toUpperCase() : val.toLowerCase();
+	arrOfValues.push({Id: index, Value: val });
+	let divWrapper = document.createElement("div");
+	divWrapper.classList.add('value');
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+	let indexEl = document.createElement("span");
+	indexEl.innerHTML = index;
+
+	let valEl = document.createElement("span");
+	valEl.innerHTML = val;
+
+	divWrapper.appendChild(indexEl);
+	divWrapper.appendChild(valEl);
+	getEl("result").appendChild(divWrapper);
+}
+
+getEl("btn").addEventListener("click", RunFunc);
